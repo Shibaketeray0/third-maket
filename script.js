92b8980cb0bc60f78a7354ac63253449a0777f8b
Denys <denishomov2015@gmail.com>
2022-07-25 09:50:42 +0300

    Project

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$('.slide').slick({
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: "<img src='images/pointer_left.png' class='prev' alt='1'>",
+    nextArrow: "<img src='images/pointer_right.png' class='next' alt='2'>",
+    autoplay: true,
+    autoplaySpeed: 2600,
+});
